b6d2b5e58839e76cace6e53382e257bc9d1c4a62
kosy318 <77595685+kosy318@users.noreply.github.com>
2023-10-23 00:48:31 +0900

    feat: reactor test

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Reactive : https://scribble-doodle-code.tistory.com/117
 Blocking I/O & Non-Blocking I/O : https://scribble-doodle-code.tistory.com/118
 
 Spring WebFlux : https://scribble-doodle-code.tistory.com/119
+
+Reactor: https://scribble-doodle-code.tistory.com/120
--- src/main/java/com/example/webfluxstudy/HelloReactor.java
+++ src/main/java/com/example/webfluxstudy/HelloReactor.java
@@ -1,11 +1,21 @@
 package com.example.webfluxstudy;
 
+import com.example.webfluxstudy.utils.Logger;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class HelloReactor {
     public static void main(String[] args) {
+        // project init test
         Mono.just("Hello Reactor")
 //                .subscribe(message -> System.out.println(message));
                 .subscribe(System.out::println);
+
+        // reactor 구성 요소 및 용어 정의
+        // Flux, Mono : Publisher
+        // just, map ... : operator
+        Flux<String> sequence = Flux.just("Hello", "Reactor"); // 데이터 생성
+        sequence.map(String::toLowerCase) // 데이터 가공
+                .subscribe(Logger::onNext);
     }
 }
